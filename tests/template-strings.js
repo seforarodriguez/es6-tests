45334a1243fe463dcdfafc92190425b43a23bcf0
seforarodriguez <seforarodriguez13@gmail.com>
2016-01-25 15:29:09 -0600

    Adding tests with string templates

--- tests/template-strings.js
+++ tests/template-strings.js
@@ -6,4 +6,28 @@ describe('template strings', function () {
 
     expect(string).to.be.a('string');
   })
+  it('can evaluate variables', function () {
+    var bar = 'baz';
+    var string = `foo${bar}`
+
+    expect(string).to.equal('foobaz');
+  })
+
+  it('can execute functions', function() {
+    var string = `the date is: ${new Date()}`;
+    var date =  new Date();
+    expect(string).to.equal('the date is: ' + date);
+  });
+
+  it('can add 1 plus 2', function() {
+     var sum = `${1 + 2}`
+     var add = `the sum of 1 + 2: ${sum}`;
+    expect(add).to.equal('the sum of 1 + 2: ' + sum);
+  });
+
+  it('can handle multiline strings', function() {
+    var string = `hello
+                 world`;
+    expect(string).to.equal('hello\n                 world');
+  })
 })
